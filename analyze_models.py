2850107273e3baf00c813c2f36163fc1e713a7d7
askofen <askofen.bsuir@hotmail.com>
2018-04-15 14:24:13 +0200

    Fix logs path

--- analyze_models.py
+++ analyze_models.py
@@ -1,3 +1,3 @@
 from analyze.logs_analyzer import LogsAnalyzer
 
-LogsAnalyzer.parse_train_log("output/logs/standard_run_1/train.log")
+LogsAnalyzer.parse_train_log("data/models/logs/ascii_run_1/train.log")
